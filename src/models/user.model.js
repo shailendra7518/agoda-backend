13493d4c1cc65ca952fa3460136e71b424b31967
shailendra kumar <shailendraftp000@gmail.com>
2022-10-31 10:19:45 +0530

    added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  first_name : { type: String, required: true },
-  last_name: { type: String, required: true },
-  avatar: { type: String, required: true },
+  first_name : { type: String, required: false },
+  last_name: { type: String, required: false },
+  avatar: { type: String, required:false},
   email:{type:String,required:true},
   password:{type:String,required:true},
 
